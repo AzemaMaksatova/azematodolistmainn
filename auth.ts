dbf761b6737631d9515b1b41ff5c99e8090f6b11
AzemaMaksatova <sanzu18haru@gmail.com>
2024-12-05 17:05:20 +0600

    todolist

--- auth.ts
+++ auth.ts
@@ -1,9 +1,9 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
-import Google from "next-auth/providers/google";
+import Facebook from "next-auth/providers/facebook";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub, Google],
+  providers: [GitHub, Facebook],
   callbacks: {
     authorized: async ({ auth }) => {
       // Logged in users are authenticated, otherwise redirect to login page
